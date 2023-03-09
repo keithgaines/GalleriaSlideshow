016f8f5c70bb0579fb61dc05aeac5cbdb53737ee
Keith Gaines <keithg@celerocommerce.com>
2023-03-09 15:01:58 -0600

    added images to the images array

--- src/images.js
+++ src/images.js
@@ -12,7 +12,84 @@ const images = [
       url: '../assets/girl-with-pearl-earring/hero-large.jpg',
       alt: 'lightbox image',
     },
+
+    {
+      id: 3,
+      url: '../assets/guernica/hero-large.jpg',
+      alt: 'lightbox image',
+    },
     
+    {
+      id: 4,
+      url: '../assets/penitent-magdalene/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 5,
+      url: '../assets/the-storm-on-the-sea-of-galilee/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 6,
+      url: '../assets/the-great-wave-off-kanagawa/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 7,
+      url: '../assets/van-gogh-self-portrait/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 8,
+      url: '../assets/the-sleeping-gypsy/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 9,
+      url: '../assets/lady-with-an-ermine/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 10,
+      url: '../assets/the-night-cafe/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 11,
+      url: '../assets/the-basket-of-apples/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 12,
+      url: '../assets/the-boy-in-the-red-vest/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 13,
+      url: '../assets/arnolfini-portrait/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 14,
+      url: '../assets/mona-lisa/hero-large.jpg',
+      alt: 'lightbox image',
+    },
+
+    {
+      id: 15,
+      url: '../assets/the-swing/hero-large.jpg',
+      alt: 'lightbox image',
+    }
   ];
   
   export default images;
